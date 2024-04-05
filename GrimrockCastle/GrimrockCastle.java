cac5e1b18861df6ec3b90c0e70e240e2ec937a85
Stephen Grenesko <122296627+sgrenesko@users.noreply.github.com>
2024-04-05 15:42:07 -0400

    4/5/24 update, text and images
    Added pixel art image for wizard class, began text for first level, implemented player choice system, and improved upon text loading systems.

--- GrimrockCastle/GrimrockCastle.java
+++ GrimrockCastle/GrimrockCastle.java
@@ -1,3 +1,5 @@
+
+//Library imports go here
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,42 +13,57 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class GrimrockCastle {
+    // Global variables and objects go here
     private static JFrame currentFrame = null;
     static Scanner scanner = new Scanner(System.in);
-    static int delaySec=32;
+    static int delaySec = 32;
+    static int pauseGame = 3000;
+
+    // Main method that has minor control logic, some text to small to keep in a
+    // file, and calls to the various game functions
     public static void main(String[] args) {
+        // Main variables and arrays go here
         String imageName, textName;
-        int classType;
+        int classType, choice1;
         String[] classArr = { "Knight", "Wizard", "Thief" };
 
-        textName = "introtext.txt";
-        delayedPrint("                Welcome to ...",50);
-        textPrint(textName);
-        delayedPrint("A text-based short RPG created by Stephen R Grenesko\nIn this game you will be exploring the ancient Castle Grimrock, wherin you will find monsters, puzzles, and more!\nTo begin, please type your name and press enter!",delaySec);
-        String userName = getName();
-        delayedPrint("Hello " + userName + "!",delaySec);
-        delayedPrint("\nWe will now choose a class for you to play as! \nPlease select one of the following classes by typing the class name and hitting enter:",delaySec);
-        textName = "classSelect.txt";
-        textPrint(textName);
-        classType = classType();
-        imageName=classPic(classType);
-        displayImage(imageName);
-        delayedPrint("You have chosen: " + classArr[classType],delaySec);
+        // Begin main method
+        // Display the intro text and title
+        textName = "titletext.txt";
+        delayedPrint("                Welcome to ...");
+        titlePrint(textName);
 
-        /*
-         * imageName = "knight_pixel.png";
-         * displayImage(imageName);
-         * try {
-         * Thread.sleep(2500);
-         * } catch (InterruptedException e) {
-         * e.printStackTrace();
-         * }
-         * imageName = "druid_sprite_ref.png"; // Assuming this is the correct image
-         * name
-         * displayImage(imageName);
-         */
+        // Asks for user to input name
+        textName = "nametext.txt";
+        fileDelayPrint(textName);
+        String userName = getName();
+        delayedPrint("Hello " + userName + "!");
+        textName = "loadScreen.txt";
+        scrollScreen(textName);
+
+        // Ask user to enter choice for their class
+        delayedPrint(
+                "\nWe will now choose a class for you to play as! \nPlease select one of the following classes by typing the class name and hitting enter:");
+        textName = "classSelect.txt";
+        titlePrint(textName);
+        classType = classType();
+        imageName = classPic(classType);
+        displayImage(imageName);
+        delayedPrint("You have chosen: " + classArr[classType]);
+        gameDelay();
+        textName = "loadScreen.txt";
+        scrollScreen(textName);
+
+        // Begin first 'level', exposition and image loading
+        delayedPrint("\n                    Welcome to ...");
+        textName = "gateTitle.txt";
+        titlePrint(textName);
+        textName = "gateText.txt";
+        fileDelayPrint(textName);
+        choice1 = gateChoice();
     }
 
+    // Stores the entered name for later use
     public static String getName() {
         String name = "";
         // Input check for the enter key to start the game
@@ -54,12 +71,15 @@ public class GrimrockCastle {
         return name;
     }
 
+    // Function that determines the internal variable for tracking your class, used
+    // in other functions
     public static int classType() {
         int classInt = 0;
+
         while (true) {
-        String input = scanner.nextLine();
-        String classIn;
-            classIn=input.toLowerCase();
+            String input = scanner.nextLine();
+            String classIn;
+            classIn = input.toLowerCase();
             switch (classIn) {
                 case "knight":
                     classInt = 0;
@@ -71,18 +91,16 @@ public class GrimrockCastle {
                     classInt = 2;
                     break;
                 default:
-                    delayedPrint("\nInvalid class choice! Please try again.",delaySec);
+                    delayedPrint("\nInvalid class choice! Please try again.");
                     continue;
             }
-        // break;
             return classInt;
         }
-        
+
     }
 
     // The displayImage function will open and display the pixel art background
     // images for each level
-    // and push that display to the front
     public static void displayImage(String imageName) {
         // Close the previous JFrame if it exists
         if (currentFrame != null) {
@@ -131,16 +149,16 @@ public class GrimrockCastle {
         currentFrame = frame;
     }
 
-    public static void textPrint(String textName) {
+    // Iterates through every line then prints out a line every .25 seconds
+    // simulating older OS loading
+    public static void titlePrint(String textName) {
         try (BufferedReader br = new BufferedReader(new FileReader(textName))) {
             String line;
 
-            // Iterates through every line then prints out a line every .25 seconds
-            // simulating older OS loading
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
                 try {
-                    Thread.sleep(250);
+                    Thread.sleep(150);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -149,11 +167,30 @@ public class GrimrockCastle {
             e.printStackTrace();
         }
     }
-    public static void delayedPrint(String message, long delayInMillis) {
+
+    public static void scrollScreen(String textName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(textName))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // A print delay that creates the scrolling text effect on print lines in the main
+    public static void delayedPrint(String message) {
         for (char c : message.toCharArray()) {
             System.out.print(c);
             try {
-                TimeUnit.MILLISECONDS.sleep(delayInMillis);
+                TimeUnit.MILLISECONDS.sleep(delaySec);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -161,13 +198,69 @@ public class GrimrockCastle {
         System.out.println();
     }
 
-    public static String classPic(int classType){
-        String filename=" ";
-        switch(classType){
+    // Logic to determine what class you chose, and what image to display
+    public static String classPic(int classType) {
+        String filename = " ";
+        switch (classType) {
             case 0:
-            filename="knight_pixel.png";
+                filename = "knight_pixel.png";
+                break;
+            case 1:
+                filename = "wizard_pixel.png";
+                break;
+            case 2:
+                filename = "thief_pixel.png";
+                break;
         }
 
         return filename;
+    }
+
+    // Creates the scrolling text effect for .txt files with large amounts of text
+    public static void fileDelayPrint(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                for (char c : line.toCharArray()) {
+                    System.out.print(c);
+                    try {
+                        Thread.sleep(delaySec); // Adding delay between characters
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(); // Move to the next line after printing each line
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void gameDelay() {
+        try {
+            Thread.sleep(pauseGame); // 10000 milliseconds = 10 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int gateChoice() {
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                delayedPrint("You kick the door, but the only outcome is a stubbed toe... Ouch!");
+                break;
+            case 2:
+                delayedPrint("The door is locked tight, and there is nowhere to go!");
+                break;
+            case 3:
+                delayedPrint("Upon closer inspection, there are loose bricks around one of the hinges...");
+                break;
+            case 4:
+                delayedPrint("You open your pack and look inside...");
+                break;
+        }
+
+        return choice;
     }
 }
